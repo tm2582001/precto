40b280e82609a609e3759cf88736aba69d0f1f67
Tushar Mehta <tm2582001@gmail.com>
2025-02-08 21:48:24 +0530

    added error handling

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,22 @@ use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
 fn main() {
     enable_raw_mode().unwrap();
     for b in io::stdin().bytes(){
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-        } else {
-            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}", b, c);
-        }
-        if c == 'q' {
-            disable_raw_mode().unwrap();
-            break;
+        match b {
+            Ok(b)=>{
+                let c = b as char;
+                if c.is_control() {
+                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+                } else {
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}", b, c);
+                }
+                if c == 'q' {
+                    disable_raw_mode().unwrap();
+                    break;
+                }
+            }
+            Err(err)=>println!("Err:{}", err)
         }
     }
+    disable_raw_mode().unwrap()
 
 }
