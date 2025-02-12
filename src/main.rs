f012c6ee9afc904a85ac136ce6a59de72e43b903
Tushar Mehta <tm2582001@gmail.com>
2025-02-12 15:37:19 +0530

    updaated logic to accept key input

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,7 @@
-use std::io::{self, Read};
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+mod editor;
+use editor::Editor;
 
 fn main() {
-    enable_raw_mode().unwrap();
-    for b in io::stdin().bytes(){
-        match b {
-            Ok(b)=>{
-                let c = b as char;
-                if c.is_control() {
-                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-                } else {
-                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}", b, c);
-                }
-                if c == 'q' {
-                    disable_raw_mode().unwrap();
-                    break;
-                }
-            }
-            Err(err)=>println!("Err:{}", err)
-        }
-    }
-    disable_raw_mode().unwrap()
-
+    let editor = Editor::default();            
+    editor.run();
 }
