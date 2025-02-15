f9be1ac4976bba134b9e71a3c77c2211c462ad2a
Tushar Mehta <tm2582001@gmail.com>
2025-02-15 18:23:11 +0530

    added logic to clear terminal

--- src/editor.rs
+++ src/editor.rs
@@ -1,29 +1,73 @@
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
-use crossterm::event::{read, Event::Key, KeyCode::Char};
+use crossterm::event::{read,Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 
-pub struct Editor {}
+use std::io::stdout;
+
+
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
     pub fn default() -> Self {
-        Editor {}
+        Editor {
+            should_quit: false
+        }
     }
 
-    pub fn run(&self) {
-        enable_raw_mode().unwrap();
+    pub fn run(&mut self) {
+        Self::initialize().unwrap();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
+    }
+
+    fn initialize()->Result<(), std::io::Error>{
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+
+    fn terminate()->Result<(), std::io::Error>{
+        disable_raw_mode()
+    }
+
+    fn clear_screen()->Result<(), std::io::Error>{
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{event:?} \r");
-                    if let Char(c) = event.code {
-                        if c == 'q' {
-                            break;
-                        }
-                    }   
-                },
-                Err(err) => println!("Error: {err}"),
-                _ => ()
+            let event = read()?;
+            self.evaluate_event(&event);
+            self.refresh_screen()?;
+
+            if self.should_quit{
+                break;
             }
         }
-        disable_raw_mode().unwrap();
+
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self,event: &Event){
+        if let Key(KeyEvent {code, modifiers, ..}) = event{
+            match code {
+                Char('c') if *modifiers == KeyModifiers::CONTROL =>{
+                                self.should_quit = true;
+                            }
+                            _ =>()
+                        }
+            }
+        }
+
+    fn refresh_screen(&self)->Result<(), std::io::Error>{
+        if self.should_quit {
+            Self::clear_screen()?;
+            print!("Goodbye.\r\n");
+        }
+
+        Ok(())
     }
 }
