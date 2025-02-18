54c900e0b417e615fe4649a86e8bd8cfae3d64b7
Tushar Mehta <tm2582001@gmail.com>
2025-02-18 14:26:05 +0530

    added message in the middle of the screen

--- src/editor.rs
+++ src/editor.rs
@@ -1,18 +1,19 @@
-use crossterm::event::{read,Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use std::io::Error;
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
 
 mod terminal;
-use terminal::Terminal;
+use terminal::{Position, Size, Terminal};
 
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor {
     should_quit: bool,
 }
 
 impl Editor {
-    pub const  fn default() -> Self {
-        Self {
-            should_quit: false
-        }
+    pub const fn default() -> Self {
+        Self { should_quit: false }
     }
 
     pub fn run(&mut self) {
@@ -22,11 +23,12 @@ impl Editor {
         result.unwrap();
     }
 
-    fn repl(&mut self) -> Result<(), std::io::Error> {
+    fn repl(&mut self) -> Result<(), Error> {
         loop {
             self.refresh_screen()?;
-            
-            if self.should_quit{
+            Terminal::execute()?;
+
+            if self.should_quit {
                 break;
             }
             let event = read()?;
@@ -36,35 +38,66 @@ impl Editor {
         Ok(())
     }
 
-    fn evaluate_event(&mut self,event: &Event){
-        if let Key(KeyEvent {code, modifiers, ..}) = event{
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+            code, modifiers, ..
+        }) = event
+        {
             match code {
-                Char('c') if *modifiers == KeyModifiers::CONTROL =>{
-                                self.should_quit = true;
-                            }
-                            _ =>()
-                        }
+                Char('c') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                }
+                _ => (),
             }
         }
+    }
 
-    fn refresh_screen(&self)->Result<(), std::io::Error>{
+    fn refresh_screen(&self) -> Result<(), Error> {
+        Terminal::hide_cursor()?;
+
         if self.should_quit {
             Terminal::clear_screen()?;
-            print!("Goodbye.\r\n");
-        }else {
+            Terminal::print("Goodbye.\r\n")?;
+        } else {
             Self::draw_rows()?;
-            Terminal::move_cursor_to(0, 0)?;
+            Terminal::move_cursor_to(Position { x: 0, y: 0 })?;
         }
 
+        Terminal::show_cursor()?;
+        Terminal::execute()?;
         Ok(())
     }
 
-    fn draw_rows()->Result<(), std::io::Error>{
-        let height = Terminal::size()?.1;
+    fn draw_welcome_message()->Result<(), Error>{
+        let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
+        let width = Terminal::size()?.width as usize;
+        let len = welcome_message.len();
+        let padding = (width - len) / 2;
+        let spaces = " ".repeat(padding - 1);
+        welcome_message = format!("~{spaces}{welcome_message}");
+        welcome_message.truncate(width);
+        Terminal::print(welcome_message)?;
+        Ok(())
+    }
+
+    fn draw_empty_row() -> Result<(), Error> {
+        Terminal::print("~")?;
+        Ok(())
+    }
+
+    fn draw_rows() -> Result<(), Error> {
+        let Size { height, .. } = Terminal::size()?;
+
         for current_row in 0..height {
-            print!("~");
+            Terminal::clear_line()?;
+            if current_row == height / 3 {
+                Self::draw_welcome_message()?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+
             if current_row + 1 < height {
-                print!("\r\n");
+                Terminal::print("\r\n")?;
             }
         }
 
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,38 +1,84 @@
-use std::io::stdout;
+use std::io::{stdout, Write, Error};
 
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType, size};
-use crossterm::execute;
-use crossterm::cursor::MoveTo;
+use crossterm::cursor::{Hide, MoveTo, Show};
+use crossterm::{queue, Command};
+use crossterm::style::Print;
+use core::fmt::Display;
+
+#[derive(Copy, Clone)]
+pub struct Size {
+    pub height: u16,
+    pub width: u16,
+}
+
+#[derive(Copy, Clone)]
+pub struct Position {
+    pub x: u16,
+    pub y: u16,
+}
 
 
-pub struct Terminal{}
+pub struct Terminal;
 
 impl Terminal {
 
-    pub fn terminate()->Result<(), std::io::Error>{
+    pub fn terminate()->Result<(), Error>{
         disable_raw_mode()?;
+        Self::execute()?;
         Ok(())
     }
 
-    pub fn initialize()->Result<(), std::io::Error>{
+    pub fn initialize()->Result<(), Error>{
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(0, 0)?;
+        Self::move_cursor_to(Position { x: 0, y: 0 })?;
+        Self::execute()?;
         Ok(())
     }
 
-
-    pub fn clear_screen()->Result<(), std::io::Error>{
-        execute!(stdout(), Clear(ClearType::All))?;
+    pub fn clear_screen()->Result<(), Error>{
+        Self::queue_command(Clear(ClearType::All))?;
         Ok(())
     }
 
-    pub fn move_cursor_to(x: u16, y: u16) -> Result<(), std::io::Error> {
-        execute!(stdout(), MoveTo(x, y))?;
+    pub fn clear_line()->Result<(),Error>{
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
         Ok(())
     }
 
-    pub fn size() -> Result<(u16, u16), std::io::Error> {
-        size()
+    pub fn move_cursor_to(position: Position) -> Result<(), std::io::Error> {
+        Self::queue_command(MoveTo(position.x, position.y))?;
+        Ok(())
+    }
+
+    pub fn hide_cursor()->Result<(), Error>{
+        Self::queue_command(Hide)?;
+        Ok(())
+    }
+
+    pub fn show_cursor()->Result<(), Error>{
+        Self::queue_command(Show)?;
+        Ok(())
+    }
+
+    pub fn print<T: Display>(string: T) -> Result<(), Error> {
+        Self::queue_command(Print(string))?;
+        Ok(())
+    }
+
+    pub fn size() -> Result<Size, Error> {
+        let (width, height) = size()?;
+        Ok(Size { height, width })
+    }
+
+    pub fn execute() -> Result<(), Error> {
+        stdout().flush()?;
+        Ok(())
+    }
+
+    fn queue_command<T:Command>(command: T) -> Result<(), Error> {
+        queue!(stdout(), command)?;
+        Ok(())
     }
 }
